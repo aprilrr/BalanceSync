14b516c6713f1048ff792debbd35f4a993c6aff2
April <60031800+aprilrr@users.noreply.github.com>
2025-12-10 21:26:27 +0700

    Add main.go
    Co-authored-by: Hajigur <66867581+hajigur69@users.noreply.github.com>

---
+++ cmd/balancesync/main.go
@@ -0,0 +1,23 @@
+// cmd/balancesync/main.go
+package main
+
+import (
+    "flag"
+    "log"
+    "os"
+
+    "balancesync/internal/balancesync"
+)
+
+func main() {
+    verbose := flag.Bool("verbose", false, "Enable verbose logging")
+    input := flag.String("input", "", "Input file path")
+    output := flag.String("output", "", "Output file path")
+    flag.Parse()
+
+    app := balancesync.NewApp(*verbose)
+    if err := app.Run(*input, *output); err != nil {
+        log.Printf("Error: %v", err)
+        os.Exit(1)
+    }
+}
